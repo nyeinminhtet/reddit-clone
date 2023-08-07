83e79ecdc8d32fc909596c3c9f36c71c6d7fc883
Two J <TwoJ@gmail.com>
2023-08-07 09:18:25 +0630

    feat/home-feed-detail-page

---
+++ src/components/post-vote/PostVoteServer.tsx
@@ -0,0 +1,51 @@
+import { Post, Vote, VoteType } from "@prisma/client";
+import { getServerSession } from "next-auth";
+import { notFound } from "next/navigation";
+import React from "react";
+import PostVoteClient from "./PostVoteClient";
+
+interface Props {
+  postId: string;
+  initialVoteAmt?: number;
+  initialVote?: VoteType | null;
+  getData?: () => Promise<(Post & { votes: Vote[] }) | null>;
+}
+
+const PostVoteServer = async ({
+  postId,
+  initialVoteAmt,
+  initialVote,
+  getData,
+}: Props) => {
+  const session = await getServerSession();
+
+  let _votesAmt: number = 0;
+  let _currentVote: VoteType | null | undefined = undefined;
+
+  if (getData) {
+    const post = await getData();
+    if (!post) return notFound();
+
+    _votesAmt = post.votes.reduce((pre, vote) => {
+      if (vote.type === "UP") return pre + 1;
+      if (vote.type === "DOWN") return pre - 1;
+      return pre;
+    }, 0);
+
+    _currentVote = post.votes.find(
+      (vote) => vote.userId === session?.user.id
+    )?.type;
+  } else {
+    _votesAmt = initialVoteAmt!;
+    _currentVote = initialVote;
+  }
+  return (
+    <PostVoteClient
+      postId={postId}
+      initialVoteAmt={_votesAmt}
+      initialVote={_currentVote}
+    />
+  );
+};
+
+export default PostVoteServer;
